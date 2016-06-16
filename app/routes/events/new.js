ec10817ed9c8e41d2d8f134d8fd2118f2043de1e
Eve <eve.essex@gmail.com>
2016-06-16 12:00:22 -0400

    add new event route and form

---
+++ app/routes/events/new.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('event');
+  },
+  actions: {
+    save() {
+      let event = this.modelFor(this.routeName);
+      event.save().then(()=>{
+        this.transitionTo('events.event', event)
+      })
+    }
+  }
+});
