c22c01ab808f232f754b5ca91ccc672b4875f234
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-17 14:38:12 -0400

    Fix edit events except for date

--- app/components/show-event.js
+++ app/components/show-event.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    update() {
+      this.get('event').save();
+      this.get('event').toggleProperty('isEditing');
+    }
+  }
 });
