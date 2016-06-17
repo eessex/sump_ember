9b8e784854d728f0260a585897bc4d687af91444
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-17 14:48:33 -0400

    Add presenter edit action and handle toggle

--- app/components/show-presenter.js
+++ app/components/show-presenter.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    update() {
+      this.get('presenter').save();
+      this.get('presenter').toggleProperty('isEditing');
+    }
+  }
 });
