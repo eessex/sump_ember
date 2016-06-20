b25e5e25bdc6141628ae8425830462ed4b0a7f98
Eve <eve.essex@gmail.com>
2016-06-19 22:26:00 -0400

    create or add existing presenter

--- app/components/all-presenters.js
+++ app/components/all-presenters.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+store: Ember.inject.service(),
   results: Ember.computed('query', function(){
      return this.get('presenters').filter((selection) =>{
-        let query = this.get('query').toLowerCase()
-        return selection.get('name').toLowerCase().includes(query)
+        let query = this.get('query').toLowerCase();
+        return selection.get('name').toLowerCase().includes(query);
       })
   }),
   actions: {
@@ -12,5 +13,13 @@ export default Ember.Component.extend({
       this.attrs.onPresenterAdd(selection);
       this.$('input').val('');
     },
+    addNewPresenter() {
+      let store = this.get('store');
+      let newPresenterName = this.$('input').val();
+      let newPresenterObject = store.createRecord('presenter');
+      newPresenterObject.set('name', newPresenterName);
+      this.attrs.onPresenterAdd(newPresenterObject);
+      this.$('input').val('');
+    }
   }
 });
