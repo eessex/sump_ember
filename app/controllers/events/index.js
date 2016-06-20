e9f0930daa296a469c6aaf1aa801200bc7eb0f7a
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-20 12:57:42 -0400

    Get sorting working for date, name, presenters on events

---
+++ app/controllers/events/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    sortByName() {
+      var sorted = this.get('model').sortBy('name');
+      this.set('model', sorted);
+    },
+    sortByDate() {
+      var sorted = this.get('model').sortBy('date');
+      this.set('model', sorted);
+    },
+    sortByPresenter() {
+      var sorted = this.get('model').sortBy('presenter.name');
+      this.set('model', sorted);
+    }
+  }
+});
