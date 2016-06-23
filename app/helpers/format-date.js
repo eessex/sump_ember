57273e58324ba4cf10eacf18e0d8eb5c78b08c0a
Eve <eve.essex@gmail.com>
2016-06-23 14:02:37 -0400

    update dates

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 export function formatDate(params) {
   var format = params[0];
   var date = params[1];
-  return moment(date).format(format);
+  return moment.utc(date).format(format);
 }
 
 export default Ember.Helper.helper(formatDate);
