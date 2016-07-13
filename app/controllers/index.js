683b709268deb030351118815437007c1ffbbd0a
Eve <eve.essex@gmail.com>
2016-07-13 11:55:14 -0400

    update mobile titles

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,10 @@ export default Ember.Controller.extend({
   upcoming: Ember.computed(function() {
     return this.get('model').filterBy('upcoming', 'true').sortBy('date');
   }),
+  upcomingCount: Ember.computed(function() {
+debugger
+     this.get('model').filterBy('upcoming', 'true').get('length');
+  }),
   past: Ember.computed(function() {
     return this.get('model').filterBy('upcoming', 'false');
   }),
